6ef07c65d7398249ed1849f03a9f4b8d71b20593
Karto4an <ikarto4an@gmail.com>
2024-01-25 15:05:58 +0200

    Refactored code for improved modularity

--- main.py
+++ main.py
@@ -16,16 +16,42 @@ with open('scheme.json', 'r', encoding='utf-8') as f:
 # {'commands': [{'name': 'start', 'description': 'Start command', 'aliases': ['start'], 'action': 'start_message_action'}], 'actions': [{'name': 'start_message_action', 'description': 'Start message action', 'proccess': {'type': 'message', 'message': 'Hello, {{username}}!'}}]}
 
 
-def register_handlers():
-    for command in scheme['commands']:
-        @bot.message_handler(commands=command['aliases'])
-        def handler(message):
-            action = command['action']
-            for action in scheme['actions']:
-                if action['name'] == command['action']:
-                    if action['proccess']['type'] == 'message':
-                        bot.send_message(message.chat.id, action['proccess']['message'].replace('{{username}}', message.from_user.username))
+def get_action(action_name):
+    """
+    Retrieve the action associated with a command from the scheme dictionary.
+    """
+    for action in scheme['actions']:
+        if action['name'] == action_name:
+            return action
+    return None
 
+def process_message(action, message):
+    """
+    Process the message based on the action's content.
+    """
+    if action['proccess']['type'] == 'message':
+        response_message = action['proccess']['message'].replace('{{username}}', message.from_user.username)
+        bot.send_message(message.chat.id, response_message)
+
+def register_handler(command):
+    """
+    Register a handler for a specific command.
+    """
+    command_aliases = command['aliases']
+    command_action = command['action']
+
+    @bot.message_handler(commands=command_aliases)
+    def handler(message):
+        action = get_action(command_action)
+        if action:
+            process_message(action, message)
+
+def register_handlers():
+    """
+    Register handlers for commands defined in the scheme dictionary.
+    """
+    for command in scheme['commands']:
+        register_handler(command)
 
 register_handlers()
 bot.polling()
